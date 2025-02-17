d146c50fe1a00e88fc0401848d0596715644e0e4
Viswa M <viswaviswa236@gmail.com>
2025-02-17 17:05:44 +0530

    Version 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    public static void countFrequency(String name){
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        for(int i=0;i<name.length();i++){
+            map.put(name.charAt(i),map.getOrDefault(name.charAt(i),0)+1);
+        }
+//        map.forEach((k,v)->System.out.println(k+" : "+v));
+        for(int i=0;i<name.length();i++) System.out.println(name.charAt(i)+" : "+map.get(name.charAt(i)));
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner in = new Scanner(System.in);
+        countFrequency(in.nextLine());
     }
 }
